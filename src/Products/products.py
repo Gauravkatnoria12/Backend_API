e26fc84b5e2a1028c8e1b0b6d3e57472aa33a588
Gauravkatnoria12 <144324982+Gauravkatnoria12@users.noreply.github.com>
2026-06-06 23:17:53 +0530

    new route - get_all_prod

--- src/Products/products.py
+++ src/Products/products.py
@@ -18,7 +18,15 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@route.post("/", status_code=status.HTTP_201_CREATED)
+
+@route.get("/", response_model=list[ProductBase], status_code=status.HTTP_200_OK)
+async def get_all_prod(db: db_dependency):
+    products = db.query(models.Products).all()
+    if products:
+        return products
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Products Not Found")
+
+@route.post("/product", status_code=status.HTTP_201_CREATED)
 async def add_prod(product : ProductBase, db: db_dependency):
   db_product = models.Products(**product.model_dump())
   db.add(db_product)
