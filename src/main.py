08ce59b6758d813fd7daa51b49dd5bed66c58442
Gauravkatnoria12 <g200007k@gmail.com>
2026-06-08 12:00:33 +0530

    fastapi-mysql

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
+from importlib.metadata import version
 from fastapi import FastAPI
 
 from Products.products import route
 
-app = FastAPI()
+v = "v1"
+
+app = FastAPI(
+    title = "FastAPI & MySQL",
+    description="A REST API for web service",
+    version = v
+)
 app.include_router(route, prefix='/Products', tags=['Products'])
 
 @app.get("/")
